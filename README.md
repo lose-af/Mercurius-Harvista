88f1cdf799119b309a198a2ecaf8dcd08ef88c0c
Nixieboluo <me@sagirii.me>
2025-09-06 20:27:01 +0800

    Initialize the pack
    Signed-off-by: Nixieboluo <me@sagirii.me>

---
+++ README.md
@@ -0,0 +1,13 @@
+# Harvista
+
+Modified version of Harvista modpack for Ourmoe Minecraft server.
+
+Original modpack: [Modrinth](https://modrinth.com/modpack/harvista)
+
+Licensed under MPL-2.0.
+
+## Changes
+
+### 3.1.2-ourmoe+r1
+
+-   Removed `Simple Voice Chat`.
---
+++ server/make_server.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+FORGE_VERSION="21.1.190"
+
+# Ensure java is installed
+if ! command -v java >/dev/null 2>&1; then
+    echo "Java 17 must be installed"
+    exit 1
+fi
+
+# Function to download with either curl or wget
+download() {
+    local url="$1"
+    local output_path="$2"
+
+    if command -v curl >/dev/null 2>&1; then
+        if [ -z "$output_path" ]; then
+            curl -OJL "$url"
+        else
+            # Create directory if it doesn't exist
+            mkdir -p "$(dirname "$output_path")"
+            curl -L "$url" -o "$output_path"
+        fi
+    else
+        echo "curl not found. Please install curl."
+        exit 1
+    fi
+}
+
+# Install (Neo)Forge
+if [ ! -f forge-$FORGE_VERSION-installer.jar ]; then
+
+    download "https://maven.neoforged.net/releases/net/neoforged/neoforge/$FORGE_VERSION/neoforge-$FORGE_VERSION-installer.jar"
+    java -jar forge-$FORGE_VERSION-installer.jar --installServer
+    rm run.sh run.bat
+fi
+
+# These mods does not allow distribution on curseforge
+mkdir -p mods
+while IFS=$'\t' read -r url filename; do
+  if [ $url == END ]; then
+    break
+  fi
+
+  # Execute the download command
+  download "$url" mods/$filename
+done < manual_mods.list
+
+# Install packwiz if needed
+if [ ! -f packwiz-installer-bootstrap.jar ]; then
+    download "https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar"
+fi
+
+java -jar packwiz-installer-bootstrap.jar -g -s server ../pack.toml
+
+echo Use the following command to start the server
+echo java @user_jvm_args.txt @libraries/net/neoforged/neoforge/$FORGE_VERSION/unix_args.txt nogui
